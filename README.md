8bc84c5c2da325f7f2986a7e8a3672043e3d87fe
Lucie Rezbarikova <lucie.rezbarikova@gmail.com>
2024-03-10 12:01:08 +0100

    done

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Doplňte do stránky JavaScriptový program, který pomocí funkce prompt požá
 Program pak nastaví posuvníky na stránce na odpovídající délku a hodnoty u posuvníků na uživatelem zadaná čísla.
 
 Na samém začátku vašeho programu vytvořte funkci updateSkill, která bere dva vstupy: textový řetězec představující id elementu a číslo mezi 0–100. 
+
 Tato funkce při zavolání nastaví posuvník se zadaným id na správnou hodnotu. Použijte tuto funkci ve vašem kódu, 
 abyste nemuseli při nastavování posuvníků psát třikrát po sobě téměř stejný kód.
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('funguju!');
+const updateSkill = (id, level) => {
+  const postup = document.querySelector(`#${id} .skill__progress`);
+  const hodnota = document.querySelector(`#${id} .skill__value`);
+  hodnota.textContent = `${level} / 100`;
+  postup.style.width = `${level}%`;
+};
+
+const skillHtml = Number(prompt('Jaká je vaše znalost HTML? (ohodnoťte na stupnici od 0 až 100)'));
+const skillCss = Number(prompt('Jaká je vaše znalost CSS? (ohodnoťte na stupnici od 0 až 100)'));
+const skillJS = Number(prompt('Jaká je vaše znalost Javascriptu? (ohodnoťte na stupnici od 0 až 100)'));
+
+updateSkill('skill1', skillHtml);
+updateSkill('skill2', skillCss);
+updateSkill('skill3', skillJS);
